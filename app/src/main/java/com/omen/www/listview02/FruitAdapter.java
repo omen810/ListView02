1bab110c0bfdbbce609387de6e0076d877c09963
omen810 <8561073@qq.com>
2016-07-24 16:35:39 +0800

    version-2016.07.24pm16

--- app/src/main/java/com/omen/www/listview02/FruitAdapter.java
+++ app/src/main/java/com/omen/www/listview02/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.omen.www.listview02;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,10 @@ import java.util.List;
 /**
  * Created by lsx on 2016/7/24.
  */
-public class FruitAdapter extends ArrayAdapter {
+/*继承ArrayAdapter的时候，要记得在后面加上泛型<Fruit>*/
+public class FruitAdapter extends ArrayAdapter<Fruit> {
     private int resourceId;
+    private int mCountOfInflater = 0;
 
     public FruitAdapter(Context context, int resource, List<Fruit> objects) {
         super(context, resource, objects);
@@ -23,12 +26,24 @@ public class FruitAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Fruit fruit=getItem(position);
-        View view= LayoutInflater.from(getContext()).inflate(resourceId,null);
+        Fruit fruit = getItem(position);
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            Log.d("FRUIT", "新造箱子: " + ++mCountOfInflater);
+        } else {
+            view = convertView;
+        }
         ImageView fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
         TextView fruitText = (TextView) view.findViewById(R.id.fruit_name);
         fruitImage.setImageResource(fruit.getImageId());
         fruitText.setText(fruit.getName());
         return view;
+    }
+
+    @Override
+    public int getCount() {
+        Log.d("FRUIT", "getCount: " + super.getCount());
+        return super.getCount();
     }
 }
--- app/src/main/java/com/omen/www/listview02/MainActivity.java
+++ app/src/main/java/com/omen/www/listview02/MainActivity.java
@@ -8,20 +8,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<Fruit> fruitList = new ArrayList<Fruit>();
+    private List<Fruit> fruitList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Fruit apple = new Fruit("apple", R.mipmap.ic_launcher);
-        fruitList.add(apple);
-        Fruit banana = new Fruit("banana", R.mipmap.ic_launcher);
-        fruitList.add(banana);
-        Fruit orange = new Fruit("orange", R.mipmap.ic_launcher);
-        fruitList.add(orange);
+        initFruits();
         FruitAdapter adapter=new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+    }
+
+    private void initFruits() {
+        String[] fruitNames={"apple", "banana", "orange", "watermelon", "pear", "grape",
+                "Pineapple", "strawberry", "cherry", "apple", "banana", "orange", "watermelon", "pear"};
+        int[] fruitImages={R.drawable.apple_pic, R.drawable.banana_pic, R.drawable.orange_pic,
+                R.drawable.watermelon_pic, R.drawable.pear_pic, R.drawable.grape_pic,
+                R.drawable.pineapple_pic, R.drawable.strawberry_pic, R.drawable.cherry_pic,
+                R.drawable.apple_pic, R.drawable.banana_pic, R.drawable.orange_pic,
+                R.drawable.watermelon_pic, R.drawable.pear_pic};
+        fruitList= new ArrayList<Fruit>();
+        for(int i=0;i<fruitNames.length;i++){
+            fruitList.add(new Fruit(fruitNames[i], fruitImages[i]));
+        }
     }
 }
